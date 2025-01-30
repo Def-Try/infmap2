b8e2144cd1b7b0216cc19b07e6c05571bdf04684
googer <googer@fractal.gov>
2025-01-30 13:47:45 +0700

    chore: remove unused loc in fog

--- lua/infmap2/icl/fog.lua
+++ lua/infmap2/icl/fog.lua
@@ -1,6 +1,5 @@
 hook.Add("SetupWorldFog", "!infmap_fog", function()
     local color = InfMap2.Visual.Fog.Color
-	local co = LocalPlayer():GetMegaPos()[3]
 	render.FogStart(InfMap2.Visual.Fog.Start)
 	render.FogMaxDensity(InfMap2.Visual.Fog.MaxDensity)
 	render.FogColor(color.r, color.g, color.b)
